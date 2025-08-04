1f68ec4d6519f7cde517e02f47d301fae18c39e9
Nicu Mamaliga <mamaliganicu2004@gmail.com>
2025-08-04 15:51:33 +0300

    added Game class to handle the game

--- src/GameClass.java
+++ src/GameClass.java
@@ -1,2 +1,117 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class GameClass {
+        private List<Question> questions  = new ArrayList<>();
+        private String fileName;
+
+        public GameClass(String fileName) throws FileNotFoundException {
+
+            this.fileName = fileName;
+        }
+        public void StartGame() throws FileNotFoundException {
+            loadQuestions() ;
+        }
+
+        private void loadQuestions() throws FileNotFoundException {
+
+            File questionsFile = new File(fileName);
+            Scanner reader = new Scanner(questionsFile);
+
+            while (reader.hasNextLine()) {
+                // read question line number and question text
+                String questionLine = reader.nextLine();
+                Scanner lineScanner= new Scanner(questionLine);
+                int questionNumber = lineScanner.nextInt();
+                String questiontext = lineScanner.hasNext()? lineScanner.nextLine().trim() : "";
+                lineScanner.close();
+
+                //read the answers
+                String answerLine = reader.nextLine();
+                List<String> answers = new ArrayList<>(3);
+                Scanner answerLineScanner = new Scanner(answerLine);
+
+                Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(answerLine);
+                while (m.find()) {
+                    answers.add(m.group(1)); // text inside parentheses
+                }
+                answerLineScanner.close();
+                int correctAnswer = Integer.parseInt(reader.nextLine().trim());
+                Question q = new Question(questionNumber, questiontext, answers, correctAnswer);
+                questions.add(q);
+            }
+            reader.close();
+
+            List<Question>groupedQuestions = new ArrayList<>(5);
+            int level = 1;
+            for(int i=0; i<questions.size();i++) {
+                groupedQuestions.add(questions.get(i));
+                if((i+1)%5==0)
+                {
+                    System.out.println("Level " + level);
+                    DisplayQuestions(groupedQuestions);
+                    level++;
+                    System.out.println();
+                    groupedQuestions.clear();
+                }
+
+            }
+            if(!groupedQuestions.isEmpty()) {
+                System.out.println("Level " + level);
+                DisplayQuestions(groupedQuestions);
+
+            }
+        }
+
+        public void DisplayQuestions(List<Question> questions) {
+            Collections.shuffle(questions);
+            int score = 0;
+            for(Question q : questions) {
+                System.out.println(q.getQuestionId() + " " + q.getQuestion() + " " + q.getAnswers());
+                System.out.println("Plase enter the answer, a number between 0, 2");
+                Scanner sc =  new Scanner(System.in);
+
+                if(checkAnswer(q)) {
+                    score++;
+                }
+                else {
+                    System.out.println("Your current score is " + score);
+                    if(score!=25) {
+                        System.out.println("Sorry you dindt become a millionare\n you want to try again?");
+                        String option;
+                        Scanner scr = new Scanner(System.in);
+                        option = scr.nextLine();
+                        if(option.equals("yes")) {
+                            DisplayQuestions(questions);
+                        }
+                        else
+                            break;
+                        scr.close();
+                    }
+                }
+            }
+        }
+
+    boolean checkAnswer(Question q)
+    {
+        Scanner sc = new Scanner(System.in);
+        int answer = sc.nextInt();
+        if(answer <0||answer>2) {
+            System.out.println("Number between 0 and 2");
+            checkAnswer(q);
+        }
+        if(answer == q.getCorrectAnswer()) {
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -25,61 +21,8 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Error creating file: " + e.getMessage());
         }
-            List<Question> questions = new ArrayList<>();
-            File questionsFile = new File(fileName);
-            Scanner reader = new Scanner(questionsFile);
+            GameClass Game = new GameClass(fileName);
+        Game.StartGame();
 
-            while (reader.hasNextLine()) {
-                // read question line number and question text
-               String questionLine = reader.nextLine();
-               Scanner lineScanner= new Scanner(questionLine);
-               int questionNumber = lineScanner.nextInt();
-               String questiontext = lineScanner.hasNext()? lineScanner.nextLine().trim() : "";
-               lineScanner.close();
-
-               //read the answers
-                String answerLine = reader.nextLine();
-                List<String> answers = new ArrayList<>(3);
-                Scanner answerLineScanner = new Scanner(answerLine);
-
-
-                Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(answerLine);
-                while (m.find()) {
-                    answers.add(m.group(1)); // text inside parentheses
-                }
-                answerLineScanner.close();
-                int correctAnswer = Integer.parseInt(reader.nextLine().trim());
-                Question q = new Question(questionNumber, questiontext, answers, correctAnswer);
-                questions.add(q);
-            }
-            reader.close();
-
-            List<Question>groupedQuestions = new ArrayList<>(6);
-            int level = 1;
-            for(int i=0; i<questions.size();i++) {
-                groupedQuestions.add(questions.get(i));
-                if((i+1)%5==0)
-                {
-                    System.out.println("Level " + level);
-                    DisplayQuestions(groupedQuestions);
-                    level++;
-                    System.out.println();
-                    groupedQuestions.clear();
-                }
-
-            }
-            if(!groupedQuestions.isEmpty()) {
-                System.out.println("Level " + level);
-                DisplayQuestions(groupedQuestions);
-
-            }
     }
-
-    private static void DisplayQuestions(List<Question> questions) {
-
-        Collections.shuffle(questions);
-        for(Question q : questions)
-           System.out.println(q.getQuestionId() + " "+ q.getQuestion() + " "+q.getAnswers() + " " + q.getCorrectAnswer());
-        }
-
 }
