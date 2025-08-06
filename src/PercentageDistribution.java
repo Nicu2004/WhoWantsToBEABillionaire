0f0b45d6586afe64350e30622f72f7af4b6364e0
Nicu Mamaliga <mamaliganicu2004@gmail.com>
2025-08-06 14:37:36 +0300

    PercentageDistribution

--- src/PercentageDistribution.java
+++ src/PercentageDistribution.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class PercentageDistribution {
+
+    public static int[] generatePercentages() {
+        Random rand = new Random();
+        int a = rand.nextInt(101);
+        int b = rand.nextInt(101-a);
+        int c = rand.nextInt(101-a-b);
+        int d = 100-a-b-c;
+        System.out.println(a+" "+b+" "+c+" "+d);
+        System.out.println(a+b+c+d);
+        return new int[]{a,b,c,d};
+    }
 }
