6097030991a802a1d444e12e4843f0dea93ce881
Nicu Mamaliga <mamaliganicu2004@gmail.com>
2025-08-07 20:41:00 +0300

    added call frined and 50%

--- src/GameClass.java
+++ src/GameClass.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,7 +19,7 @@ public class GameClass {
         }
 
         public void StartGame() throws FileNotFoundException {
-            System.out.printf("Welcome %s to Who Wants To Be A Millionare\n", player.name());
+            System.out.printf("Welcome %s to Who Wants To Be A Millionaire\n", player.name());
             loadQuestions() ;
         }
         private void loadQuestions() throws FileNotFoundException {
@@ -30,7 +31,7 @@ public class GameClass {
                 String questionLine = reader.nextLine();
                 Scanner lineScanner= new Scanner(questionLine);
                 int questionNumber = lineScanner.nextInt();
-                String questiontext = lineScanner.hasNext()? lineScanner.nextLine().trim() : "";
+                String questionText = lineScanner.hasNext()? lineScanner.nextLine().trim() : "";
                 lineScanner.close();
 
                 //read the answers
@@ -44,7 +45,7 @@ public class GameClass {
                 }
                 answerLineScanner.close();
                 int correctAnswer = Integer.parseInt(reader.nextLine().trim());
-                Question q = new Question(questionNumber, questiontext, answers, correctAnswer);
+                Question q = new Question(questionNumber, questionText, answers, correctAnswer);
                 questions.add(q);
             }
             reader.close();
@@ -76,17 +77,17 @@ public class GameClass {
 
             int score = 0;
             for(Question q : questions) {
+                System.out.println(ConsoleColors.RED+"Please enter the answer, a number between 1 and 4\n  or a to ask the audience, c to call a friend or f to make it 50% 50%"+ConsoleColors.RESET);
+
                 System.out.println(q.getQuestionId() + " " + q.getQuestion() + " " + q.getAnswers()+"\n");
-                System.out.println(ConsoleColors.RED_BACKGROUND_BRIGHT+"Plase enter the answer, a number between 1 and 4\n"+ConsoleColors.RESET);
-
-
                 if(checkAnswer(q)) {
+                    System.out.println(ConsoleColors.GREEN_BOLD+"Correct!"+ConsoleColors.RESET);
                     score++;
                 }
                 else {
                     System.out.println("Your current score is " + score);
                     if(score!=25) {
-                        System.out.println("Sorry you dindt become a millionare\n you want to try again?");
+                        System.out.println("Sorry you didn't become a millionaire\n you want to try again?");
                         String option;
 
                         option = sc.nextLine();
@@ -109,24 +110,22 @@ public class GameClass {
         int option=-1;
         switch (answer) {
             case "a" -> askAudience(q);//ask audience
-            case "c" -> callFriend();//call a friend
-            case "f" -> make50_50();//make 50/50;
+            case "c" -> System.out.println(callFriend(q));//call a friend
+            case "f" -> option = make50_50(q);//make 50/50;
             case "1", "2", "3", "4" -> option = Integer.parseInt(answer) ;
-            default -> System.out.println("Invalid option");
-        };
+            default -> {
+                System.out.println("Invalid option please select a number if you know the answer \n between 1 - 4 or a to ask the audience, c to call a friend or f to make it 50% 50%");
+                checkAnswer(q);
+            }
+        }
         if(option==-1) {
-            System.out.println("plase Select the option");
-            int varinat = sc.nextInt();
-            return varinat - 1 == q.getCorrectAnswer();
+            int variant = sc.nextInt();
+            return variant - 1 == q.getCorrectAnswer();
         }
         else return option -1 == q.getCorrectAnswer();
     }
-    void askAudience(Question q)
-    {
-        int[] audienceResponse =  PercentageDistribution.generatePercentages();
-        swapInList(audienceResponse,q.getCorrectAnswer());
-    }
-    void swapInList(int[] newAudience, int correctIndex)
+
+    void distributeAudienceCorrectAnswer(int[] newAudience, int correctIndex)
     {
         int maxIndex = 0;
 
@@ -142,20 +141,45 @@ public class GameClass {
         System.out.println(Arrays.toString(newAudience));
 
     }
-    int getOption(String option, Question q)
+    //methods to ask for help
+    void askAudience(Question q)
     {
-        return switch (option) {
-            case "a" -> 1;
-            case "c" -> 2;
-            case "f" -> 3;
-            case "1", "2", "3", "4" -> 0;
-            default -> -1;
-        };
+        System.out.println("Asking audience for the answer...");
+        int[] audienceResponse =  PercentageDistribution.generatePercentages();
+        distributeAudienceCorrectAnswer(audienceResponse,q.getCorrectAnswer());
     }
-    void callFriend()
-    {}
-    void make50_50()
-    {}
 
-
+    int callFriend(Question q)
+    {
+        Random rand = new Random();
+        int chance = rand.nextInt(100);
+        if(chance<90){
+            chance = q.getCorrectAnswer()+1;
+        }
+        else
+        {
+            do {
+                chance = rand.nextInt(3)+1;
+            }while(chance==q.getCorrectAnswer());
+        }
+        return chance;
+    }
+    int make50_50(Question q)
+    {
+        List<String> fiftyLine = new ArrayList<>(2);
+        int correctAnswer = q.getCorrectAnswer();
+        Random rand = new Random();
+        int randomAnswer;
+        do{
+            randomAnswer = rand.nextInt(q.getAnswers().size());
+        }while(randomAnswer==correctAnswer);
+        fiftyLine.add(q.getAnswers().get(randomAnswer));
+        fiftyLine.add(q.getAnswers().get(correctAnswer));
+        Collections.shuffle(fiftyLine);
+        System.out.println(Arrays.toString(fiftyLine.toArray()));
+        correctAnswer -=2;
+        if(correctAnswer<0)
+            correctAnswer = 0;
+        return correctAnswer;
+    }
 }
